fad6126f34937a6ebb08b70fa22147b3f64b9481
2022_i_1_Vashche <vital10@inbox.ru>
2022-12-20 21:54:55 +0300

    Tasks 1 and 2

---
+++ Task1.js
@@ -0,0 +1,43 @@
+const xmlStudents = `
+<list>
+  <student>
+    <name lang="en">
+      <first>Ivan</first>
+      <second>Ivanov</second>
+    </name>
+    <age>35</age>
+    <prof>teacher</prof>
+  </student>
+    <student>
+     <name lang="ru">
+       <first>Петр</first>
+       <second>Петров</second>
+     </name>
+     <age>58</age>
+     <prof>driver</prof>
+   </student>
+</list>`;
+const parser = new DOMParser();
+
+const xmlDOMListStudents = parser.parseFromString(xmlStudents, 'text/xml');
+const studentsList = xmlDOMListStudents.querySelector('list');
+const firstStudentNode = studentsList.firstElementChild;
+const firstStudentNameNode = firstStudentNode.querySelector('name');
+const FirstStudentfirstName = firstStudentNameNode.querySelector('first');
+const FirstStudentSecondName = firstStudentNameNode.querySelector('second');
+const firstStudentAge = firstStudentNode.querySelector('age');
+const firstStudentProf = firstStudentNode.querySelector('prof');
+
+const firstStudentNameAttr = firstStudentNameNode.getAttribute('lang');
+
+const result = {
+  list: [
+    {
+      name: `${FirstStudentfirstName.textContent} ${FirstStudentSecondName.textContent}`,
+      age: firstStudentAge.textContent,
+      prof: firstStudentProf.textContent,
+      lang: firstStudentNameAttr,
+    },
+  ],
+};
+console.log(result);
---
+++ Task2.js
@@ -0,0 +1,34 @@
+const jsonString = `{
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }`;
+const data = JSON.parse(jsonString);
+const list = data.list;
+const firstListItem = list[0];
+const secondListItem = list[1];
+
+const result = {
+  list: [
+    {
+      name: firstListItem.name,
+      age: firstListItem.age,
+      prof: firstListItem.prof,
+    },
+    {
+      name: secondListItem.name,
+      age: secondListItem.age,
+      prof: secondListItem.prof,
+    },
+  ],
+};
+console.log('result', result);
